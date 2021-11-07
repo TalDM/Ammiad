dab9f6ce808fcd0fa26b9b4383cd124f54ded36b
Tal Dahan Meir <42034440+TalDM@users.noreply.github.com>
2021-11-07 12:58:30 +0200

    Update identity.r

--- identity.r
+++ identity.r
@@ -46,8 +46,8 @@ covered_frac = apply(covered,2,function(x){100*(sum(x=="cov")/length(x))})
 covered_sum = apply(covered,2,function(x){sum(x=="cov")})
 names(covered_frac) = colnames(covered)
 
-sort(covered_frac)
-sort(covered_sum)
+#sort(covered_frac)
+#sort(covered_sum)
 
 
 ### calculate pairwise identity ###
@@ -57,3 +57,56 @@ similarity.matrix<-apply(vcf_tab_clean_0_3,2,function(x)colSums(x==vcf_tab_clean
 Ncomparisons_matrix=apply(vcf_tab_clean_0_3,2,function(x)colSums(!is.na(x==vcf_tab_clean_0_3)))
 identity.matrix=similarity.matrix/Ncomparisons_matrix
 
+                          
+### plot pairwise identity distribution of Ammiad (removing Zavitan controls) ###
+                          
+no_z=identity.matrix[!grepl(as.character("Zavitan"),rownames(identity.matrix)),!grepl("Zavitan",colnames(identity.matrix))]
+#hist(no_z[lower.tri(no_z)],breaks=60,col="#a1d99b",border="#74C476")
+   
+                          
+### for every plant, find all plants that has at least X (identity_threshold) identity to it ###
+                          
+require(igraph)
+require(data.table)
+                          
+identity_threshold=0.981
+my_list=list()
+for (i in 1:ncol(identity.matrix))
+{
+relevant_vector = rownames(identity.matrix)[identity.matrix[,i] >= identity_threshold]
+my_list[[i]]=relevant_vector
+}
+names(my_list) = colnames(identity.matrix)
+
+
+
+overlap <- function(u, v) length(intersect(u, v)) / min(length(u), length(v)) > 0.8
+adj <- sapply(my_list, function(u) sapply(my_list, overlap, u)) + 0
+g <- graph_from_adjacency_matrix(adj)
+memb <- components(g)$membership
+s <- split(my_list, memb)
+groups <- lapply(s, function(x) unique(unlist(x)))
+cat(paste0("getting info from ",length(groups), " groups\n"))
+#length(groups)
+
+                 
+### assign names to groups ###
+                 
+names(groups) <- paste("G",1:length(groups),sep="")
+lengths=as.vector(sapply(groups, length))
+#sort(lengths)
+
+                 
+### make table with groups and levels ### 
+                 
+m1=max(lengths(groups))
+d1=as.data.frame(do.call(rbind,lapply(groups,'length<-',m1)),stringsAsFactors=FALSE)
+names(d1)=paste0("Level",seq_along(d1))
+csv=d1
+ group_sizes = apply(csv,2,function(x){sum(!is.na(x))})
+
+groups_vector = c()
+for (i in 1:length(group_sizes)){
+groups_vector = c(groups_vector,rep(i,group_sizes[i]))}
+groups_list = apply(csv,2,function(x){as.character(x)[!is.na(x)]})
+names(groups_vector) = unlist(groups_list,use.names=FALSE)
