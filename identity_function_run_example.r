3dccb22560f6995f401aafaf4ad4f2c4d7f717f3
Tal Dahan Meir <42034440+TalDM@users.noreply.github.com>
2021-11-07 13:26:31 +0200

    Create identity_function_run_example.r

---
+++ identity_function_run_example.r
@@ -0,0 +1,22 @@
+#########################
+### load the function ###
+
+source("identity_function.r")
+
+
+### run it with specific files: ###  
+
+amiad_summarize_vcf(vcf_path = "845.vcf",
+					map_csv_path = "map.csv",
+					output_csv_path = "output.csv",
+					identity_threshold = 0.981)
+
+
+### assign colors to DGGs ### 
+
+ammiad_colorize_by_groups(summarized_table_path="output.csv",
+    colors_table_path="DGG_colors.csv",
+		output_csv_path="output_with_colors.csv")
+
+
+#########################
