9147231c76cb640fe8a074b76ecd4f574db1b826
Tal Dahan Meir <42034440+TalDM@users.noreply.github.com>
2021-11-08 11:45:26 +0200

    Update identity_function_run_example.r

--- identity_function_run_example.r
+++ identity_function_run_example.r
@@ -23,8 +23,8 @@ ammiad_summarize_vcf(vcf_path = "845.vcf",
 ### assign colors to DGGs ### 
 
 ammiad_colorize_by_groups(summarized_table_path="output.csv",
-    colors_table_path="DGG_colors.csv",
-		output_csv_path="output_with_colors.csv")
+                          colors_table_path="DGG_colors.csv",
+		          output_csv_path="output_with_colors.csv")
 
 
 #########################
