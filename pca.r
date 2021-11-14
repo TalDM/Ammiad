7141097cd8e2d544f16a5f216a976df01074c7e5
Tal Dahan Meir <42034440+TalDM@users.noreply.github.com>
2021-11-14 11:09:47 +0200

    Update pca.r

--- pca.r
+++ pca.r
@@ -17,24 +17,22 @@ ccm_pca<-snpgdsPCA(genofile)
 ### load output file to get DGG colors ###
 data=read.csv("data/output_with_colors.csv")
 
-
 ### habitat colors vector ###
 colors=c("#2a7fff","#f7a8b8","#ff5555","#ff9955","#5fd35f","#ffe680","#aa0000","#9955ff")
 
-tab_identity=data.frame(sample.id=data$Sample, pop=factor(data$Habitat), dgg_col=factor(data$color), EV1=ccm_pca$eigenvec[,1],EV2=ccm_pca$eigenvec[,2],EV3=ccm_pca$eigenvec[,3],EV4=ccm_pca$eigenvec[,4], stringsAsFactors=FALSE)
-
+tab_identity=data.frame(sample.id=data$Sample, pop=factor(data$Habitat_North_divided), igg_col=factor(data$color), EV1=ccm_pca$eigenvec[,1],EV2=ccm_pca$eigenvec[,2], stringsAsFactors=FALSE)
+tab_identity=tab_identity[!grepl("Zavitan",tab_identity$sample.id),]
 
 ### plot pca coloring by habitat ###
 
-pdf("20210825_pc34_snprelate_by_habitat.pdf",height=8,width=8)
-plot(tab_identity$EV3, tab_identity$EV4, col=colors[as.integer(tab_identity$pop)],xlab="PC1(5.76% explained var.)",ylab="PC2(4.93% explained var.)",pch=20)
-legend("topright", legend=levels(tab_identity$pop), pch=19, col=colors)
+pdf("20210830_pc12_snprelate_by_habitat.pdf",height=8,width=8)
+plot(tab_identity$EV1, tab_identity$EV2, col=colors[as.integer(tab_identity$pop)],xlab="PC1(18.14% explained var.)",ylab="PC2(8.02% explained var.)",pch=20)
+legend("bottomright", legend=levels(tab_identity$pop), pch=19, col=colors)
 dev.off()
 
 
 ### plot pca coloring by DGG ###
 
-pdf("20210825_pc12_snprelate_by_habitat.pdf",height=8,width=8)
-plot(tab_identity$EV1, tab_identity$EV2, col=colors[as.integer(tab_identity$pop)],xlab="PC1(18.14% explained var.)",ylab="PC2(8.02% explained var.)",pch=20)
-legend("bottomright", legend=levels(tab_identity$pop), pch=19, col=colors)
+pdf("20210830_pc12_snprelate_by_igg.pdf",height=8,width=8)
+plot(tab_identity$EV1, tab_identity$EV2, col=as.character(tab_identity$igg_col),xlab="PC1(18.14% explained var.)",ylab="PC2(8.02% explained var.)",pch=20)
 dev.off()
